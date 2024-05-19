0ef8a58cf227cf6e11163766606ee8e5e6282f01
freyacevansss <freyacevans@outlook.com>
2024-05-19 15:14:57 +1000

    Used partial Code for Media Player

---
+++ week 8/script.js
@@ -0,0 +1,34 @@
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPaueImg);
+
+playPaueButton.addEventListener("click", toggleVideoPlayback);
+
+function toggleVideoPlayback() {
+  if (myVideo.paused || myVideo.ended) {
+    playPaueImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+    myVideo.play();
+  } else {
+    playPaueImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
+    myVideo.pause();
+  }
+}
+const muteUnmuteButton = document.querySelector("#mute-unmute-button");
+console.log(muteUnmuteButton);
+
+muteUnmuteButton.addEventListener("click", toggleSound);
+
+function toggleSound() {
+  if (myVideo.muted) {
+    muteUnmuteButton.stylle.backgroundColor = "blue";
+    myVideo.muted = false;
+  } else {
+    muteUnmuteButton.style.backgroundColor = "red";
+    myVideo.muted = true;
+  }
+}
